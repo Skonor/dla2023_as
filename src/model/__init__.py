7ebb7493f8153d1e9816b00e21e68082b894f353
Skonor <gorbunovmikh73@gmail.com>
2023-12-13 12:11:43 +0300

    working rawnet2

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,6 +1,8 @@
 from src.model.dummy_model import DummyModel
+from src.model.rawnet2 import RawNet2
 
 
 __all__ = [
     "DummyModel",
+    "RawNet2"
 ]
---
+++ src/model/layers/__init__.py
@@ -0,0 +1,7 @@
+from src.model.layers.SincLayerRawnet2 import SincConv_fast as SincConv
+from src.model.layers.SincLayerSincNet import SincConv_fast as SincConvSincNet
+
+__all__ = [
+    "SincConv",
+    "SincConvSincNet"
+]
